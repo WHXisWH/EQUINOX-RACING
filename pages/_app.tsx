e0fa51ddde63f1900c6ccf499b241e8c030c058e
WHXisWH <whx12110405@gmail.com>
2025-08-04 14:32:03 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
+import { AptosWalletAdapterProvider, type AvailableWallets } from '@aptos-labs/wallet-adapter-react';
 import '@/styles/globals.css';
 import '@aptos-labs/wallet-adapter-ant-design/dist/index.css';
 
 
-const wallets = ['Petra'];
+const wallets: Readonly<AvailableWallets[]> = ['Petra'];
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
